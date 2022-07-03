f814034c752ae3c0885565d486125407bddcae8d
Qqinzijing <101794254+Qqinzijing@users.noreply.github.com>
2022-07-03 18:57:44 +0800

    仿写Dart Web异步、HTML、DOM等示例

---
+++ 02-hello_web/02_web/web/add-elements.dart
@@ -0,0 +1,16 @@
+// https://dart.dev/tutorials/web/low-level-html/add-elements
+
+import 'dart:html';
+
+final InputElement MenuInput1 = querySelector('#Menu-input1') as InputElement;
+final UListElement MenuList1 = querySelector('#Menu-list1') as UListElement;
+
+void main() {
+  MenuInput1.onChange.listen(addMenuItem);
+}
+
+void addMenuItem(Event e) {
+  final newList1 = LIElement()..text = MenuInput1.value;
+  MenuInput1.value = '';
+  MenuList1.children.add(newList1);
+}
